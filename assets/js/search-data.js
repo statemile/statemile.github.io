f7a07f82762f75ba4bb813ac254fb617e7dfe20f
statemile <87587718+statemile@users.noreply.github.com>
2026-01-30 13:29:57 +0000

    Deploying to gh-pages from @ statemile/statemile.github.io@fbd7a176d68ddd79fcc39721c5cb0c2aedfd568d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,20 +3,13 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-home",
+    title: "Home",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
